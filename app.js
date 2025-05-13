8d226098fad33ba7d9906bc92741a86bc4173d2d
OctFog <xix063@ucsd.edu>
2025-05-12 17:53:52 -0700

    Updated app.js and feature view category

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let categories = [];
+
 app.get('/homepage', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/homepage.html'));
 });
 
 app.get('/category_creator', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/category_creator.html'));
+});
+
+app.get('/category_view', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/category_view.html'));
 });
 
 app.post('/add_category', (req, res) => {
@@ -25,13 +31,28 @@ app.post('/add_category', (req, res) => {
   let flex = req.body.flex
   if (!cType || cType.trim() === '') {
     return res.status(400).send('<h2>Error:</h2><p>Category Name is required.</p>');
-  }
-  let newCat = new Category(cType, red, green, blue, flex)
-      res.send(`
-              <h2>Category Created:</h2>
-              <pre>${JSON.stringify(newCat, null, 2)}</pre>
-            `);
+  } 
+  const newCategory = {
+    cType,
+    red,
+    green,
+    blue
+    // tasks: tasks || [], // Default to an empty array if tasks are not provided
+    // description: description || ''
+  };
+
+  categories.push(newCategory);
+
+  res.status(200).send(`
+    <h2>Category Created:</h2>
+    <pre>${JSON.stringify(newCategory, null, 2)}</pre>
+  `);
 });
+
+app.get('/categories', (req, res) => {
+  res.status(200).json(categories); // Return categories as JSON
+});
+
 
 const print = function(s) {
   console.log(s);
